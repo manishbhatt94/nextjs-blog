b0e80e04db1e18c4b7a278eea0f7df931368374d
Manish Bhatt <manishbhatt594@gmail.com>
2025-06-30 21:54:05 +0530

    Add post details page

---
+++ pages/posts/[id].js
@@ -0,0 +1,31 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.id}
+      <br />
+      {postData.title}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
